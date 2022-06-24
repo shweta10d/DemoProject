85d01b2af95c6b2b865ece09555e79d822a38eab
Deshpande <shweta.v.deshpande@accenture.com>
2022-06-24 11:19:19 +0530

    commit First file

---
+++ TestProjCode.java
@@ -0,0 +1,10 @@
+package ext.test;
+
+public class TestProjCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to DevOps Demo");
+	}
+
+}
